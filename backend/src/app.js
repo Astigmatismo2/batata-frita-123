ee00d85bd88ac76d97cb89cc4bfc27081c7a70a8
Astigmatismo2 <miguelfigueredodias@gmail.com>
2026-01-18 12:53:46 -0300

    Add files via upload

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const uri = process.env.MONGODB_URI;
 
 app.use(express.json());
 app.use(((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://astigmatismo2.github.io/batata-frita-123/');
+  res.header('Access-Control-Allow-Origin', 'https://astigmatismo2.github.io');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 }
@@ -22,16 +22,38 @@ let mensagens = Mensagem.find({mensagem: {$exists: true}}).then(docs => {
 });
 
 mongoose.connect(uri)
-  .then(() => console.log('MongoDB connected'))
+  .then(() => {
+    console.log('MongoDB connected');
+
+  let mensagens = Mensagem.find({mensagem: {$exists: true}}).then(docs => {
+    mensagens = docs;
+  }).catch(err => {
+    console.error(err);
+  });
+})
   .catch(err => console.error('MongoDB connection error:', err));
 
 app.post('/mensagens', (req, res) => {
   const mensagem = req.body;
-  mensagens.push(mensagem);
+  const novaMensagem = new Mensagem(mensagem);
+
+  novaMensagem.save().then(() => {
+    console.log(mensagem.mensagem);
+  }).catch(err => {
+    console.error('Erro ao salvar mensagem:', err);
+  });
+  
   res.status(201).send(mensagem);
 });
 
 app.get('/mensagens', (req, res) => {
+
+  let mensagens = Mensagem.find({mensagem: {$exists: true}}).then(docs => {
+    mensagens = docs;
+  }).catch(err => {
+    console.error(err);
+  });
+
   res.send(mensagens);
 });
 
